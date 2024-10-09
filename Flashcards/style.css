df5089238a993aaed11bc08a96d00f91cc0d720c
nickmartins07 <162316649+nickmartins07@users.noreply.github.com>
2024-10-09 09:29:24 -0300

    ssds

---
+++ Flashcards/style.css
@@ -0,0 +1,18 @@
+Body {
+    background-color: azure;
+    padding: 10px;
+    text-align: center;
+    justify-content: center;
+    height: auto;
+}
+.js, .css{
+    background-color: bisque;
+    border: 1px solid black ;
+    border-radius: 2px;
+}
+footer {
+    background-color: black;
+    color: white;
+    padding: 1rem;
+    font-size: 8px;
+}
